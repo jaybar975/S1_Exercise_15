a87136bd12748d47bd170076e15fa306eea4cbfb
Justin Aybar <jaybar975@west-mec.org>
2019-04-17 15:24:36 -0700

    3rd Commit Push
    Added more variables, functions, if else statements, if statements, for loops, and else if statements

--- co_payment.js
+++ co_payment.js
@@ -52,12 +52,108 @@ window.addEventListener("load", function () {
 
       // Write the field values to the order form
       document.forms.order.elements.orderDate.value = formFields[1];
-      document.forms.order.elements.orderDate.value = formFields[5];
-      document.forms.order.elements.orderDate.value = formFields[7];
-      document.forms.order.elements.orderDate.value = formFields[9];
-      document.forms.order.elements.orderDate.value = formFields[13];
-      document.forms.order.elements.orderDate.value = formFields[15];
-      document.forms.order.elements.orderDate.value = formFields[17];
-      document.forms.order.elements.orderDate.value = formFields[19];
-      document.forms.order.elements.orderDate.value = formFields[21];
+      document.forms.order.elements.modelName.value = formFields[5];
+      document.forms.order.elements.qty.value = formFields[7];
+      document.forms.order.elements.initialCost.value = formFields[9];
+      document.forms.order.elements.protectionName.value = formFields[13];
+      document.forms.order.elements.protectionCost.value = formFields[15];
+      document.forms.order.elements.subtotal.value = formFields[17];
+      document.forms.order.elements.salesTax.value = formFields[19];
+      document.forms.order.elements.totalCost.value = formFields[21];
 });
+
+window.addEventListener("load", function () {
+      document.getElementById("subButton").onclick = runSubmit;
+      document.getElementById("cardName").oninput = validateName;
+      document.getElementById("expMonth").onchange = validateMonth;
+      document.getElementById("expYear").onchange = validateYear;
+      document.getElementById("cvc").oninput = validateCVC;
+});
+
+function runSubmit() {
+      validateName();
+      validateCredit();
+      validateNumber();
+      validateMonth();
+      validateYear();
+      validateCVC();
+}
+
+function validateCVC() {
+      var cardCVC = document.getElementById("cvc");
+      var creditCard = document.querySelector('input[name="credit"]:checked').value;
+
+      if (cardCVC.validity.valueMissing) {
+            cardCVC.setCustomValidity("Enter your CVC number");
+      } else if ((creditCard === "amex") && (/^\d{4}$/.test(cardCVC.value) === false)) {
+            cardCVC.setCustomValidity("Enter a 4-digit CVC number")
+      } else if ((creditCard !== "amex") && (/^\d{3}$/.test(cardCVC.value) === false)) {
+            cardCVC.setCustomValidity("Enter a 3-digit CVC number");
+      } else {
+            cardCVC.setCustomValidity("");
+      }
+}
+
+function validateMonth() {
+      var cardMonth = document.getElementById("expMonth");
+      if (cardMonth.selectedIndex === 0) {
+            cardMonth.setCustomValidity("Select the expiration month");
+      } else {
+            cardMonth.setCustomValidity("");
+      }
+}
+
+function validateYear() {
+      var cardYear = document.getElementById("expYear");
+      if (cardYear.selectedIndex === 0) {
+            cardYear.setCustomValidity("Select the expiration year");
+      } else {
+            cardYear.setCustomValidity("");
+      }
+}
+
+function validateNumber() {
+      var cardNumber = document.getElementById("cardNumber");
+      if (cardNumber.validity.valueMissing) {
+            cardNumber.setCustomValidity("Enter your card number");
+      } else if (cardNumber.validity.patternMismatch) {
+            cardNumber.setCustomValidity("Enter a valid card number");
+      } else {
+            cardNumber.setCustomValidity("");
+      }
+}
+
+function validateCredit() {
+      var creditCard = document.forms.payment.elements.credit[0];
+      if (creditCard.validity.valueMissing) {
+            creditCard.setCustomValidity("Select your credit card")
+      } else {
+            creditCard.setCustomValidity("");
+      }
+}
+
+function validateName() {
+      var cardName = document.getElementById("cardName");
+      if (cardName.validity.valueMissing) {
+            cardName.setCustomValidity("Enter your name as it appears on the card");
+      } else {
+            cardName.setCustomValidity("");
+      }
+}
+
+function sumDigits(numStr) {
+      var digitTotal = 0;
+      for (let i = 0; i < numStr.length; i++) {
+            digitTotal += parseInt(numStr.charAt(i));
+
+      }
+      return digitTotal;
+}
+
+function luhn(idNum) {
+      // Retrieve the odd number digits 
+
+      // Retrieve the even number digits
+
+      // Retrieve whether the sum of the digits is divisible by 10
+}
